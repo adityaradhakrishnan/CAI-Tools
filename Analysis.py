8525ac51bfb4a57a7b1a1b089ef0b14396fa4216
Aditya Radhakrishnan <radhakrishnan@jhu.edu>
2015-08-10 17:46:03 -0400

    Added a whole bunch of Analysis to the workflow.

--- Analysis.py
+++ Analysis.py
@@ -60,15 +60,19 @@ LengthDict = {
     'chrXVI':948066,     'chrM':85779
 }
 
-ProfilingSamples = ['../WIGs/FC278-L5-P1-CAGATC-DhOE','../WIGs/FC278-L5-P1-TGACCA-DhKO','../WIGs/FC278-L6-P1-ACTTGA-WT']
-RNASeqSamples    = ['../WIGs/HBGN1ADXX-1-CAGATC-DhOE','../WIGs/HBGN1ADXX-1-TGACCA-DhKO','../WIGs/HBGN1ADXX-2-ACTTGA-WT']
+#ProfilingSamples = ['../WIGs/FC278-L6-P1-ACTTGA-WT','../WIGs/FC278-L5-P1-TGACCA-DhKO','../WIGs/FC278-L5-P1-CAGATC-Old-DhOE']
+#RNASeqSamples    = ['../WIGs/HBGN1ADXX-2-ACTTGA-WT','../WIGs/HBGN1ADXX-1-TGACCA-DhKO','../WIGs/HBGN1ADXX-1-CAGATC-DhOE']
 
-DensityDict = {}
+#ProfilingDensityDict = {}
+#RNASeqDensityDict    = {}   
 
-for Idx in Samples:
-	print Idx
-	DensityDict[Idx] = LoadDict(Idx, LengthDict)
-		
+#for Idx in ProfilingSamples:
+	#print Idx
+	#ProfilingDensityDict[Idx] = LoadDict(Idx, LengthDict)
+
+#for Idx in RNASeqSamples:
+	#print Idx
+	#RNASeqDensityDict[Idx]    = LoadDict(Idx, LengthDict)		
 
 # Generate a list of all the gene sequences
 
@@ -80,24 +84,37 @@ for line in File:
 	Genes[Name] = Gene(line, GODict, Expression, HalfLives)
 	Genes[Name].DefineSeq(Genome[Genes[Name].chr])
 	Genes[Name].CalculateCAI(CAIDict)
-	Genes[Name].ProfilingDensity(DensityDict, ProfilingSamples)
-	Genes[Name].RNASeqDensity(DensityDict, RNASeqSamples)
+	#Genes[Name].ProfilingDensity(ProfilingDensityDict, ProfilingSamples)
+	#Genes[Name].RNASeqDensity(RNASeqDensityDict, RNASeqSamples)
 
 File.close()
 
-FileOut = open('Test.txt','w')
+High = ['YMR230W','YBR181C','YKL056C','YJL190C','YML026C','YOR063W','YPL090C','YEL034W','YHL015W','YLR325C','YNL067W','YDL191W','YLL024C','YOL121C','YDR450W','YGL135W','YGL123W','YNL302C','YOR293W','YGR254W','YDL083C','YGL147C','YOL086C','YKL152C','YHR010W','YIL052C','YDR385W','YOR369C','YGL103W','YGR192C','YPR080W','YPL131W','YGL030W','YIL018W','YLR029C','YBR118W','YJL138C','YKL180W']
+All  = ['YLR355C','YJR123W','YDR447C','YHR174W','YMR230W','YBR181C','YKL056C','YJL190C','YML024W','YML026C','YLR167W','YOR063W','YPL090C','YOL040C','YLL045C','YBR189W','YDR382W','YEL034W','YHL015W','YLR325C','YLR134W','YHR141C','YLR075W','YKL060C','YOL039W','YPR102C','YCR012W','YOR133W','YNL067W','YGL031C','YDL191W','YAL003W','YBL092W','YLR340W','YLL024C','YOL121C','YML063W','YDR450W','YLR061W','YBR031W','YPR043W','YOL127W','YPR132W','YDR012W','YPL249C-A','YGL076C','YGL135W','YNL209W','YJL189W','YGL123W','YNL302C','YOR293W','YNL069C','YMR142C','YGR254W','YDL083C','YDR418W','YGL189C','YIL069C','YGL147C','YOL086C','YKL152C','YPL198W','YHR010W','YDR064W','YIL052C','YHL033C','YOR096W','YDR385W','YGR148C','YAL038W','YOR369C','YGL103W','YGR192C','YJR009C','YJL052W','YPR080W','YLR249W','YPL131W','YPL220W','YKR059W','YMR116C','YGL030W','YDR050C','YLR110C','YNL178W','YCR031C','YLR044C','YER074W','YIL018W','YGR118W','YOL120C','YLR029C','YDR500C','YBR118W','YGL008C','YLR048W','YJL138C','YDL229W','YKL180W']	
+	
+for Idx in All:
+	if Idx  in High:
+		print Idx
+			
+#FileOut = open('Test.txt','w')
 
-for IdxG in Genes:
-	FlagCheck = 1.0
-	for Idx in xrange(0,3):
-		FlagCheck = FlagCheck*Genes[IdxG].profRPKM[Idx]*Genes[IdxG].mRNARPKM[Idx]
-	if FlagCheck == 1.0:
-		if IdxG[0] == 'Y' or IdxG[0] == 'Q':
-			FileOut.write(IdxG + '\t' + '{0:.1f}'.format(Genes[IdxG].CAI) + '\t')
-			for Idx in xrange(0,3):
-				FileOut.write(str(Genes[IdxG].profRPKM[Idx]) + '\t' + str(Genes[IdxG].mRNARPKM[Idx]) + '\t')
+#FileOut.write('Gene\tCAI\tWTProf\tWTRNA\tKOProf\tKORNA\tOEProf\tOERNA\n')
+
+#for IdxG in Genes:
+	#FlagCheck = 1.0
+	#for Idx in xrange(0,3):
+		#FlagCheck = FlagCheck*Genes[IdxG].profRPKM[Idx]*Genes[IdxG].mRNARPKM[Idx]
+	#if FlagCheck != 0.0:
+		#if IdxG[0] == 'Y' or IdxG[0] == 'Q':
+			#if Genes[IdxG].CAI < 0.3:
+				#FileOut.write(IdxG + '\t0.3\t')
+			#else:
+				#FileOut.write(IdxG + '\t' + '{0:.1f}'.format(Genes[IdxG].CAI) + '\t')
+			#for Idx in xrange(0,3):
+				#FileOut.write(str(Genes[IdxG].profRPKM[Idx]) + '\t' + str(Genes[IdxG].mRNARPKM[Idx]) + '\t')
 		
-			FileOut.write('\n')
-print time.time() - Start
+			#FileOut.write('\n')
+			
+#print time.time() - Start
 
 	
--- Analysis/Dhh1 Enrichment/LabelBoxPlot.R
+++ Analysis/Dhh1 Enrichment/LabelBoxPlot.R
@@ -1,6 +1,6 @@
 # function for number of observations 
 give.n <- function(x){
-  return(c(y = median(x)*1.05, label = length(x))) 
+  return(c(y = median(x)*0.97, label = length(x))) 
   # experiment with the multiplier to find the perfect position
 }
 
@@ -9,5 +9,48 @@ mean.n <- function(x){
   return(c(y = median(x)*0.97, label = round(mean(x),2))) 
   # experiment with the multiplier to find the perfect position
 }
+
+library(ggplot2)
+
+Dat      <- read.table('Test.txt',header=TRUE)
+
+WTRO     <- Dat$WTProf/Dat$WTRNA
+KORO     <- Dat$KOProf/Dat$KORNA
+OERO     <- Dat$OEProf/Dat$OERNA
+CAI      <- Dat$CAI
+
+KOEnhance <- KORO/WTRO
+OEEnhance <- OERO/WTRO
+
+ROFrame <- data.frame(CAI,WTRO,KORO,OERO)
+EnFrame <- data.frame(CAI,KOEnhance,OEEnhance)
+
+Dat.RNA  <- melt(Dat, id.vars = 'CAI', measure.vars=c('WTRNA','KORNA','OERNA'))
+Dat.Prof <- melt(Dat, id.vars = 'CAI', measure.vars=c('WTProf','KOProf','OEProf'))
+Dat.RO   <- melt(ROFrame, id.vars = 'CAI', measure.vars=c('WTRO','KORO','OERO'))
+Dat.En   <- melt(EnFrame, id.vars = 'CAI', measure.vars=c('KOEnhance','OEEnhance'))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = OEEnhance)) + geom_violin() + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 OE Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = 0.5, size=4) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = KOEnhance)) + geom_violin() + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 KO Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = 0.5, size=4) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = OEEnhance)) + geom_boxplot() + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 OE Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = -0.4, size=3) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = KOEnhance)) + geom_boxplot() + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 KO Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = 1.4, size=3) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = OEEnhance)) + geom_boxplot(outlier.size = 1) + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 OE Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = -0.4, size=3) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = KOEnhance)) + geom_boxplot(outlier.size = 1) + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 KO Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = 1.4, size=3) + theme(text = element_text(size=16))
+
+ggplot(EnFrame, aes(x = factor(CAI), y = KOEnhance)) + geom_boxplot() + scale_y_log10(limits = c(0.075,15)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy (Dhh1 KO Relative to WT)') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = 1.4, size=3) + theme(text = element_text(size=16))
+
+
+
+ggplot(Dat.RNA) + geom_boxplot(aes(x = factor(CAI), y = value, color = variable)) + scale_y_log10() + theme_bw() + labs(x = 'CAI', y = 'mRNA Seq (RPKM)')
+ggplot(Dat.Prof) + geom_boxplot(aes(x = factor(CAI), y = value, color = variable)) + scale_y_log10() + theme_bw() + labs(x = 'CAI', y = 'mRNA Seq (RPKM)')
+ggplot(Dat.RO) + geom_boxplot(aes(x = factor(CAI), y = value, color = variable)) + scale_y_log10() + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy')
+ggplot(EnFrame, aes(x = factor(CAI), y = KOEnhance)) + geom_boxplot(outlier.colour = 'grey80', outlier.size = 1) + scale_y_log10(limits = c(0.05,20)) + theme_bw() + labs(x = 'CAI', y = 'Ribosome Occupancy') + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = -1.5, size = 3) + stat_summary(fun.data = mean.n, geom='text', fun.y = mean, colour = 'red', vjust = 1.5, size = 3)
+
+
 
 ggplot(a, aes(factor(V2), V4/V5)) + geom_boxplot() + scale_y_log10(limits = c(0.01,100)) + stat_summary(fun.data = give.n, geom = "text", fun.y = median, vjust = -0.5, size = 3) + stat_summary(fun.data = mean.n, geom='text', fun.y = mean, colour = 'red', vjust = 1.5, size = 3) + labs(x='CAI', y = 'Enrichment in Ribosome Footprings in OE/WT')
